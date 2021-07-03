749ffd55900b9da1bf4ddadd8e6d5b8a73ff3673
An Nguyen <ikaros.spacespeaker@gmail.com>
2021-07-03 12:12:20 +0700

    Change target name

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AwesomeAdCore",
-            targets: ["AwesomeAdCoreTarget"]),
+            targets: ["AwesomeAdCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AwesomeAdCoreTarget",
+            name: "AwesomeAdCore",
             dependencies: [],
             path: "Sources",
             resources:[
@@ -30,6 +30,6 @@ let package = Package(
             ]),
         .testTarget(
             name: "AwesomeAdCoreTests",
-            dependencies: ["AwesomeAdCoreTarget"]),
+            dependencies: ["AwesomeAdCore"]),
     ]
 )
